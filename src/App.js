f6c9ce8db01bdc322382db1a886c0248d512702e
Tony Adams <notkelloggstony@gmail.com>
2021-09-29 17:03:04 -0400

    Added basic routing, need to add other page component and style navigation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { Route, Switch, Link } from 'react-router-dom';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <nav>
+          <ul id="navigation">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      {/* <Route path="/about">
+        <About />
+      </Route> */}
+    </Switch>
     </div>
   );
 }
